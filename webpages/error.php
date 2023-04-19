0166e1a71959e3c37bafb0e96fb78af095f61cde
Kyle Cedric "KC" Macapagal Medina <kylecedricmm01@yahoo.com>
2023-04-19 11:54:28 -0700

    fixed and added to error and staff page
    not sure what to do and still need to change some stuff

--- webpages/error.php
+++ webpages/error.php
@@ -1,4 +1,56 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Self Webpage Project (New Version) </title>
+    <link rel="stylesheet" href="homepage.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"/>
+ <style>
+     section {
+         padding: 60px 0;
+     }
+ </style>
 
+</head>
+<body>
+
+    <!-- navbar -->
+   <nav class="navbar navbar-expand-md navbar-light" style="background-color:#F08080;">
+       <div class="container-xxl">
+           <a href="/webpages/index.html" class="navbar-brand">
+               <span class="fx-bold text-secondary">
+                   FREERIDE : The (not-so) Secret Personality Test!
+               </span>
+           </a>
+           <!-- toggle button for mobile nav -->
+           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="#main-nav" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+           </button>
+
+           <!-- navbar links -->
+           <div class="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+               <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/webpages/about.html">About the Action-RPG</a>
+                 </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/webpages/staff.html">Meet the Staff </a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/webpages/characters.html"> Meet the Player and Spirits </a>
+                   </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#downloads"> Download the Playtest or Add it to your wishlist now!</a>
+                </li>
+                <li class="nav-item ms-2 d-none d-md-inline">
+                    <a class="btn btn-secondary" href="#downloads"> Download the Playtest or Join the Cult on Discord!</a>
+                </li>
+
+               </ul>
+           </div>
+       </div>
+   </nav>
   
     <!-- Error Page -->
     <p style="text-align:center;"><img src="/assets/images/Error-Gif.gif" alt="Dennis , a character thats a door saying this page is an Error" ></p>
---
+++ webpages/staff.php
@@ -0,0 +1,120 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Self Webpage Project (New Version) </title>
+    <link rel="stylesheet" href="homepage.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"/>
+ <style>
+     section {
+         padding: 60px 0;
+     }
+ </style>
+
+</head>
+<body>
+
+    <!-- navbar -->
+   <nav class="navbar navbar-expand-md navbar-light" style="background-color:#F08080;">
+       <div class="container-xxl">
+           <a href="/webpages/index.html" class="navbar-brand">
+               <span class="fx-bold text-secondary">
+                   FREERIDE : The (not-so) Secret Personality Test!
+               </span>
+           </a>
+           <!-- toggle button for mobile nav -->
+           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="#main-nav" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+           </button>
+
+           <!-- navbar links -->
+           <div class="collapse navbar-collapse justify-content-end align-center" id="main-nav">
+               <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/webpages/about.html">About the Action-RPG</a>
+                 </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/webpages/staff.html">Meet the Staff </a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="/webpages/characters.html"> Meet the Player and Spirits </a>
+                   </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#downloads"> Download the Playtest or Add it to your wishlist now!</a>
+                </li>
+                <li class="nav-item ms-2 d-none d-md-inline">
+                    <a class="btn btn-secondary" href="#downloads"> Download the Playtest or Join the Cult on Discord!</a>
+                </li>
+
+               </ul>
+           </div>
+       </div>
+   </nav>
+
+ <!-- About The Company -->
+ <p style="text-align:center;"><img class="img-fluid" src="/assets/images/logo.png" alt="logo"></p>
+ 
+
+   <h1 class="text-center"> About Flightyfelon Games </h1>
+   <p class="text-center">Flightyfelon Games is an up-and-coming video game studio founded in Winnipeg, led by wife & husband combo: Rebecca & Zachary Harrison.</p>
+   <br>
+   <br>
+    <p class="text-center">Our studio's goal is to become a long-term part of the Canadian game development ecosystem, focusing on sustainable work practices and crafting unique, introspective experiences.</p>
+    <br>
+    <br>
+        <p class="text-center"> Freeride, our flagship game, is a Personality Test that plays like an Action-RPG. Join our mailing list for monthly updates! </p>
+<br>
+<br>
+<br>
+<br>
+<!-- The Staff and Their Jobs or Positions -->
+
+<h1 class="text-center"> About The Staff </h1>
+<p class="text-center">Our team is composed entirely of Winnipeg locals (plus a San Diegan in California LOL).
+</p>
+<p class="text-center">Each of us brings something valuable to the studio:</p>
+
+<!-- Carousel of the Staff -->
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="..." class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>First slide label</h5>
+          <p>Some representative placeholder content for the first slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Second slide label</h5>
+          <p>Some representative placeholder content for the second slide.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Third slide label</h5>
+          <p>Some representative placeholder content for the third slide.</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+
+  <!-- Other Games from Flightyfelon Games -->
